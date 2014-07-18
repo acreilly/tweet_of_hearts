d6c4bbfce4f5f52680f0eadaea2ca209790bc0e1
Allie Reilly <acreilly3@gmail.com>
2014-07-18 11:12:49 -0700

    style2

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ActiveRecord::Base
   has_many :relationships
   has_many :followers, through: :relationships
 
-  has_many :followings, through: :followee_relationships, source: :user
   has_many :followee_relationships, :foreign_key => "follower_id", :class_name => "Relationship"
+  has_many :followings, through: :followee_relationships, source: :user
 end
