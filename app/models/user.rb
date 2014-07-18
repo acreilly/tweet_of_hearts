9b2049517ee4246523a0da5c7e3be1e7bd317efe
Jeuel Wilkerson <jeuel@Jeuels-MacBook-Pro.local>
2014-07-17 19:48:04 -0700

    database and users now has proper followers function

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ActiveRecord::Base
   has_many :tweets
   validates :email, :first_name, :password, presence: true
   validates :email, uniqueness: true
+
+  has_many :relationships
+  has_many :followers, through: :relationships
+
+  has_many :followee_relationships, :foreign_key => "follower_id", :class_name => "Relationship"
+  has_many :followings, through: :followee_relationships, source: :user
 end
