201741ae41f1932072e27e34e3ff29f7e27d33d8
Ariel Fogel <fogeltine@gmail.com>
2014-07-18 15:46:06 -0700

    test for unique email address in user sign up

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -3,25 +3,32 @@ require 'spec_helper'
 describe User do
   context "validations" do
 
-    let(:params) {{email: nil, first_name: nil, password: nil}}
     it "doesn't add a record to the db" do
+      params= {email: nil, first_name: 'ariel', password: '123456'}
       user = User.new(params)
       expect{user.save}.to_not change {User.count}
       expect(user).to be_invalid
     end
 
     it "displays the correct error message, 'can't be blank'" do
+      params= {email: nil, first_name: 'ariel', password: '123456'}
       user = User.create(params)
       expect(user.errors.full_messages).to include("Email can't be blank")
     end
 
+    it "only accepts valid email addresses" do
+      params= {email: '1', first_name: 'ariel', password: '123456'}
+      expect{User.create(params)}.to_not change {User.count}
+      params[:email] = 'ariel@dbc.com'
+      expect{User.create(params)}.to change {User.count}.by(1)
+      # pending
+    end
+
     it "only creates records with unique email addresses" do
+      params= {email: 'ariel@dbc.com', first_name: 'ariel', password: '123456'}
       pending
     end
 
-    it "only accepts valid email addresses" do
-      pending
-    end
 
     it "has a password with minimum length of 6 chars" do
       pending
